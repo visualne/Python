af559c4a6c5cb1217d4e92b946072b34d461166f
visualne <gcauer00@gmail.com>
2022-08-26 20:58:36 -0400

    Fixed number of vertical lines, added middle of mat

--- exercises/exercise60.py
+++ exercises/exercise60.py
@@ -6,12 +6,22 @@
 N=7
 M=21
 
+# 0->1
+# 1->3 (+2)
+# 2->5 (+3)
+# 3->7 (+4)
+# 4->9 (+5)
+# 5->11 (+6)
+# 6->13 (+7)
+
 def generate_symbols(line_num):
+
+    symbol = '.|.'
 
     if line_num == 0:
         number_of_symbols = 1
     else:
-        number_of_symbols = line_num + 2
+        number_of_symbols = (line_num + 1) + line_num
 
     sample_string = ''
     for something in range(number_of_symbols):
@@ -19,15 +29,39 @@ def generate_symbols(line_num):
 
     return sample_string
 
-symbol = '.|.'
+
+#  Finding middle of doormat
+if N % 2 == 0:
+    middle = N / 2
+else:
+    middle = (N / 2) + 1
+
+print(N)
+print(middle)
+
 
 for val in range(N):
 
     #  Generating symbols
     symbols_generated = generate_symbols(val)
 
-    #  Printing vertical dashes on front and back of line.
-    number_of_horizontal_dashes = (M - len(generate_symbols(val))) / 2
-    horizontal_dashes = number_of_horizontal_dashes*'-'
+    #  Checking for the middle of the mat and printing
+    #  Welcome
+    if val == middle - 1:
+        number_of_dashes = (M - 7) / 2
+        dashes = number_of_dashes * '-'
+        print(dashes+'WELCOME'+dashes)
 
-    print(horizontal_dashes+symbols_generated+horizontal_dashes)
+    #  Getting everything after middle of mat.
+    if (val <= middle - 2):
+        #  Printing vertical dashes on front and back of line.
+        number_of_horizontal_dashes = (M - len(generate_symbols(val))) / 2
+        horizontal_dashes = number_of_horizontal_dashes*'-'
+
+        print(horizontal_dashes+symbols_generated+horizontal_dashes)
+    else:
+        pass
+
+
+
+
